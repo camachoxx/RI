7962239972628c7d44ee49e3573fcfb3118d8e65
João Camacho <46079860+camachoxx@users.noreply.github.com>
2019-10-10 14:43:22 +0100

    Add files via upload

--- Project/RetrievalModelsMatrix.py
+++ Project/RetrievalModelsMatrix.py
@@ -27,21 +27,25 @@ class RetrievalModelsMatrix:
         #numero de tokens no corpus
         self.total_tokens_corpus = np.sum(self.docLen)
         
-        #frequencia de cada token no corpus ---> Mc(t)
+        #probabilidade de cada token no corpus ---> P(t|Mc)
         self.Mc = np.sum(self.tf,axis=0) / self.total_tokens_corpus
         
         
         ############ LMJM statistics ############
-        self.doc_col=vectorizer.get_feature_names()
-        self.doc_df=pd.DataFrame(tf,columns=self.doc_col)
+        #probabilidade de uma palavra num doc ---> P(t|Md)
+        self.Md = self.tf / (self.docLen[:,None] + 0.0001)
         
         
         ############ BM25 statistics ############
 
         
-    def score_vsm(self, query):
+    def score_vsm(self, query, query_to_vector=True):
         
-        query_vector = self.vectorizer.transform([query]).toarray()
+        if query_to_vector:
+            query_vector = self.vectorizer.transform([query]).toarray()
+        else:
+            query_vector = query
+            
         query_norm = np.sqrt(np.sum(np.power(query_vector, 2), axis=1))
 
         doc_scores = np.dot(query_vector, self.tfidf.T) / (0.0001 + self.docNorms * query_norm)
@@ -50,9 +54,12 @@ class RetrievalModelsMatrix:
         return doc_scores
 
     
-    def score_lmd(self, query, miu):
+    def score_lmd(self, query, miu, query_to_vector=True):
         
-        query_vector = self.vectorizer.transform([query]).toarray()
+        if query_to_vector:
+            query_vector = self.vectorizer.transform([query]).toarray()
+        else:
+            query_vector = query
         
         #probabilidades ---> P(t|Md,Mc)
         p = (self.tf + miu*self.Mc)/(self.docLen[:,None] + miu)
@@ -66,52 +73,60 @@ class RetrievalModelsMatrix:
         return doc_scores
     
 
-    def score_lmjm(self, query, lam):
+    def score_lmjm(self, query, lam, query_to_vector=True):
         
-        query_vector = self.vectorizer.transform([query]).toarray()
-        a=self.tf
-        b=self.docLen[:,None]
-        #probabilidades
-        with np.errstate(divide='ignore', invalid='ignore'):
-            Pd = np.true_divide( a, b )
-            Pd[ ~ np.isfinite( Pd )] = 0  # -inf inf NaN
-        #Pd = self.tf / self.docLen[:,None]
-        Pc = self.Mc
+        if query_to_vector:
+            query_vector = self.vectorizer.transform([query]).toarray()
+        else:
+            query_vector = query
 
         #elevar ao vetor da query (query_vector)
-        Ed = np.power(Pd, query_vector)
-        Ec = np.power(Pc, query_vector)
+        Ed = np.power(self.Md, query_vector)
+        Ec = np.power(self.Mc, query_vector)
         
         #produto das probabilidades
         doc_scores = np.longdouble(np.prod(lam*Ed + (1-lam)*Ec, axis=1))
         
         return doc_scores
         
-    """    
-    def lmjm_aux(self,y,pd,pc):
-        v=pd*y+pc*(1-y)
-        return v
-    
-    def score_lmjm(self, query,y,d):
-        #Only works for d document for now
-        query_df=pd.DataFrame(self.vectorizer.transform([query]).toarray() , columns=self.doc_col)
-        cl=query_df[query_df!=0].dropna(axis=1).columns
-        mul=1
-        for i in query_df[query_df!=0].dropna(axis=1).columns:
-                print(i)
-                Pd=np.sum(self.doc_df.loc[d,i])/np.sum(self.doc_df.iloc[d,:])
-                print(Pd)
-                Pc=np.sum(self.doc_df.loc[:,i])/self.total_tokens_corpus
-                print(Pc)
-                mul=mul*self.lmjm_aux(y,Pd,Pc)
-                
-        return mul
-    """                                                           
-    
-                                                                   
+        
     def score_bm25(self, query):
+        """a=self.tf
+        b=self.docLen[:,None]
+        #probabilidades
+        with np.errstate(divide='ignore', invalid='ignore'):
+            Pd = np.true_divide( a, b )
+            Pd[ ~ np.isfinite( Pd )] = 0  # -inf inf NaN
+        #Pd = self.tf / self.docLen[:,None]"""
         return 0
 
-    def scoreRM3(self, query):
-        return 0
+    
+    def score_rm3(self, query, alfa, top_docs, model, parameter, query_to_vector=True):
+        """i: indica qual o modelo inicial:  i=1 --> lmjm"""
+        
+        if query_to_vector:
+            query_vector = self.vectorizer.transform([query]).toarray()
+        else:
+            query_vector = query
+            
+        #doc_scores
+        initial_scores = model(query_vector, parameter, query_to_vector=False)
+        
+        #ordena os doc_scores do menor para o maior
+        sorted_scores = np.sort(initial_scores)
+        
+        #minimo das probabilidades dos documentos relevante
+        threshold = sorted_scores[-1*top_docs]
+        
+        #documentos mais relevantes
+        best_scores = initial_scores * (initial_scores >= threshold)
+        
+        #probabilidade no modelo RM1
+        P_RM1 = np.sum(self.Md * best_scores[:,None], axis=0)
+        
+        new_query = (1-alfa)*query_vector + alfa*P_RM1
+        
+        doc_scores = f(new_query, lam, query_to_vector=False)
+        
+        return doc_scores
 
